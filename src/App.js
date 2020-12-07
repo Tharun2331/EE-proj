149ea31f3aed56125e0397eb85f669c5db37bf56
Tharun sekar <tharunsd23@gmail.com>
2020-12-07 19:49:16 +0530

    version-2.0

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import ReactNotifications from 'react-notifications-component';
 //  const promise= loadStripe("pk_test_51HD4EoIwAVryXs1m20uDSxFq6l61GNrYKQeJJtxE9t8X5GGBqCmk5WXIN9aqRQjOZC6M1JpZvgBD13Tu1dhSkrIF00i1kVop36"); 
 
  function App() {
-  const [dispatch] = useStateValue();
+  const [{},dispatch] = useStateValue();
   useEffect(() => {
  
 
@@ -38,7 +38,7 @@ import ReactNotifications from 'react-notifications-component';
         });
       }
     });
-  }, );
+  },[]);
 
   return (
     
