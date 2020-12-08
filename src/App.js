8db8381f29bfb91fc7b10104522c844eaf96be95
overlordiam <64598584+overlordiam@users.noreply.github.com>
2020-12-08 09:32:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{ useEffect} from "react";
 import './App.css';
-import Header from "./Components/Header/Header"
+import Header from "./Components/Header/Header"                                                          //hi
 import Home from "./Components/Home/Home";
 import {BrowserRouter as  Router,Switch,Route } from "react-router-dom";
 import Checkout from "./Components/Checkout/Checkout";
